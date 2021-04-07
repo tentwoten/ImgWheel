e52d362d8b9e0b1247a8fc8e168ee685d4bdd7e0
a <a>
2021-04-07 20:42:14 +0800

    Revert "ImgWheel Demon v1.0"
    This reverts commit 1e2cf97dc7b9c75735b8307ca6211f9bd8518712.

--- sources/FnCore/Log_xx.cpp
+++
@@ -1,6 +0,0 @@
-﻿#include "Log_xx.h"
-using namespace std;
-void PrintInfo(const std::string& info)
-{
-	cout << info << "\r\n";
-}
--- sources/FnCore/Net.h
+++
@@ -1,9 +0,0 @@
-﻿#ifndef _NET_H_
-#define _NET_H_
-#include "ImgMatAux.hpp"
-#include "ImgMatHelper.h"
-
-
-
-
-#endif
